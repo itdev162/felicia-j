c091fa8f1c3132fba0bdff6fd0403feb52207464
Felicia J <jarosinf@gmatc.matc.edu>
2020-12-13 12:28:08 -0600

    updated postlist item handler

--- my-app/src/components/PostList/PostListItem.js
+++ my-app/src/components/PostList/PostListItem.js
@@ -27,7 +27,7 @@ const PostListItem = props => {
             </div>
             <div className="postControls">
                 <button onClick={() => deletePost(post)}>Delete</button>
-                <button onClick={() => editPost(post)}>Edit</button>
+                <button onClick={() => handleEditPost(post)}>Edit</button>
             </div>
         </div>
     );
